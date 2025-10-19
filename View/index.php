a92ae21c3bf11a34846de8e20c2e2b814203d960
Mateus Barros Brasil <mateusbarrosb0@gmail.com>
2025-10-18 21:44:57 -0300

    tests css da tabela de contatos

--- View/index.php
+++ View/index.php
@@ -15,27 +15,57 @@ include_once("layout/searchContact.php");
 
 $readContact = new ListController;
 $_SESSION['read'] = $readContact->showContacts();
-
+include("layout/table_bar.php");
 foreach ($readContact->showContacts() as $info) {
-    echo $info['nome'] ."<br>";
-    echo $info['email'] ."<br>";
-    echo $info['telefone'] ."<br>";
-    echo "<form action='edit.php' method='POST'>
+    // echo $info['nome'] ."<br>";
+    // echo $info['email'] ."<br>";
+    // echo $info['telefone'] ."<br>";
+    // echo "<form action='edit.php' method='POST'>
     
-        <input type='hidden' value=".$info['id']." name='id'>
-        <input type='submit' value='Editar'>
+    //     <input type='hidden' value=".$info['id']." name='id'>
+    //     <input type='submit' value='Editar'>
         
-        </form>";
+    //     </form>";
 
-    echo "<form action='../Controllers/ListController.php' method='POST'>
-        <input type='hidden' value='".$info['id']."' name='id'>
-        <input type='hidden' name ='action' value='deleteSubmit'>
-        <input type='submit' value='Deletar'>
+    // echo "<form action='../Controllers/ListController.php' method='POST'>
+    //     <input type='hidden' value='".$info['id']."' name='id'>
+    //     <input type='hidden' name ='action' value='deleteSubmit'>
+    //     <input type='submit' value='Deletar'>
         
-        </form>";
-    echo "<hr>";
-}   
+    //     </form>";
+    // echo "<hr>";
+    
+        
+    echo ' 
+        <tr class="tr border-white border-b-2">
+            <td class="px-10 py-10 text-xl">'. $info['nome'] . '</td>
+            <td class="px-10 py-10 text-xl">'.$info['email'].'</td>
+            <td class="px-10 py-10 text-xl">'.$info['telefone'].'</td>
+            <td>
+                <form action="edit.php" method="POST">
+                <input type="hidden" value='.$info['id'].' name="id">
+                <input type="submit" class=" mx-7.5 py-2.5 px-2.5 text-xl text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" value="Editar">
+                </form>
+            
+            </td>
+            <td>
+                <form action="../Controllers/ListController.php" method="POST">
+                <input type="hidden" value='.$info['id'].' name="id">
+                <input type="hidden" name ="action" value="deleteSubmit">
+                <input type="submit" class="mx-7.5 px-2.5 py-2.5 text-xl focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" value="Deletar">
+                </form>
+            </td>             
+            
+        </tr>
 
+    ';
+} 
+?>
+  
+</table>
+    </div>
+
+<?php
 echo "<div>
     <a href='newContact.php'>Adicionar um novo contato</a>
 </div>";
---
+++ View/layout/table_bar.php
@@ -0,0 +1,16 @@
+ <div class="table-class rounded-3xl mt-5 ml-6 dark:bg-gray-500 ">
+        <h1 class=" pb-5 text-center text-white text-5xl rounded-t-3xl! bottom-px bg-gray-700 border-b-1">
+            <span class="material-symbols-outlined text-5xl!">
+                person_book
+            </span>Contatos
+        </h1>
+    <table class="">
+        <!-- Cabeçalho -->
+        <thead class="text-2xl text-gray-800 dark:bg-gray-700">
+            <tr class="table-auto">
+                <th class="px-8 py-3 "><img class="relative top-8 -left-5" src="../src/person.png" alt="" width="30px">Nome</th>
+                <th class="px-8 py-3"><img class="relative top-8 left-8" src="../src/email.png" alt="" width="30px">Email</th>
+                <th class="px-8 py-3" colspan="3"><img class="relative top-8 md:-left-8 -left-5" src="../src/phone.png" alt="telefone" width="30px"><span class="relative md:right-35 right-33"> Telefone</span> </th>
+                <!-- <th></th> <th></th>    -->
+            </tr>
+        </thead>
